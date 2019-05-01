a99614a1739180866ba350c9fbcd40962fe8b2bf
x <739056672@qq.com>
2019-05-01 22:52:43 +0800

    Add models

--- server/admin.py
+++ server/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Task, Trial
 # Register your models here.
+
+admin.register(Task)
+admin.register(Trial)
--- server/models.py
+++ server/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    """Task model."""
+    task_id = models.AutoField(primary_key=True)
+    create_time = models.IntegerField()
+    start_time = models.IntegerField()
+    end_time = models.IntegerField()
+    data_path = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    model_names = models.CharField(max_length=100, choices=(('0', 'RandomForest'),))
+    hyper_parameters = models.TextField()
+
+
+class Trial(models.Model):
+    """trial model"""
+    task_id = models.IntegerField()
+    trial_id = models.AutoField(primary_key=True)
+    start_time = models.IntegerField()
+    end_time = models.IntegerField()
+    status = models.CharField(max_length=100)
+    model_name = models.CharField(max_length=100)
+    hyper_parameters = models.TextField()
